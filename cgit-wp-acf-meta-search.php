e80f60240276d9ffe77a39709b5270f447f71a89
Andy Reading <andy@castlegateit.co.uk>
2016-06-21 16:29:21 +0100

    Added support for repeaters and nested repeaters.

--- cgit-wp-acf-meta-search.php
+++ cgit-wp-acf-meta-search.php
@@ -5,7 +5,7 @@
 Plugin Name: Castlegate IT WP ACF Custom Field Search
 Plugin URI: http://github.com/castlegateit/cgit-wp-dradis
 Description: Allows searching of custom fields and meta values.
-Version: 1.0
+Version: 1.1
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
 License: MIT
--- src/Cgit/AcfMetaSearch.php
+++ src/Cgit/AcfMetaSearch.php
@@ -23,7 +23,14 @@ class AcfMetaSearch
      *
      * @var array
      */
-    private $search_fields = [];
+    private $searchable_fields = [];
+
+    /**
+     * Searchable meta_key values.
+     *
+     * @var array
+     */
+    private $meta_keys = [];
 
     /**
      * Constructor.
@@ -33,6 +40,7 @@ class AcfMetaSearch
     private function __construct()
     {
         $this->buildFieldList();
+        $this->buildMetaKeyList();
         $this->applyFilters();
     }
 
@@ -57,32 +65,62 @@ class AcfMetaSearch
      */
     private function buildFieldList()
     {
-        // Ensure the ACF functions are available
+        // Ensure the ACF functions are available.
         if (!function_exists('acf_local') || !isset(acf_local()->fields)) {
             return;
         }
 
-        // Available fields
+        // Available fields.
         $this->fields = acf_local()->fields;
 
-        // Searchable fields
-        $searchable_fields = array_filter(
+        // Filter down to searchable fields only.
+        $this->searchable_fields = array_filter(
             $this->fields,
             function ($a) {
-                if (isset($a['searchable']) && $a['searchable']) {
-                    return true;
-                }
+                return isset($a['searchable']) && $a['searchable'];
             }
         );
+    }
 
-        // Extract field names
-        $this->searchable_fields = array_values(
-            array_map(
-                function ($a) {
-                    return $a['name'];
-                },
-                $searchable_fields
-            )
+    /**
+     * Takes the ACF field array and constructs a regular expression to match
+     * each searchable field in the postmeta table. ACF stores each field name
+     * individually, however repeaters and nested repeaters create meta_keys
+     * which combine the field key, and parent keys such as:
+     *
+     * parent_name_0_subfield_name_0_another_subfield.
+     *
+     * A regular expression is created to match such meta_keys and all unique
+     * searchable meta keys are queried from the database.
+     *
+     * @return void
+     */
+    private function buildMetaKeyList()
+    {
+        // Build the regular expressions for each field.
+        foreach ($this->searchable_fields as $key => $field) {
+            $this->searchable_fields[$key]['regex'] = '^'.$this->getMetaKeyRegex($field) . '$';
+        }
+
+        //Find a true list of all available searchable meta_key values.
+        global $wpdb;
+
+        $sql = "SELECT DISTINCT meta_key FROM " . $wpdb->prefix . "postmeta WHERE ";
+
+        foreach ($this->searchable_fields as $field) {
+            $sql.= "meta_key REGEXP '" . $field['regex'] . "' OR ";
+        }
+
+        $sql = substr($sql, 0, -4);
+
+        $results = $wpdb->get_results($sql);
+
+        // Take the query results and populate an of searchable meta_keys
+        $this->meta_keys = array_map(
+            function ($a) {
+                return $a->meta_key;
+            },
+            $results
         );
     }
 
@@ -109,7 +147,7 @@ class AcfMetaSearch
                 $meta = ['relation' => 'OR'];
 
                 // Add each field
-                foreach ($this->searchable_fields as $field) {
+                foreach ($this->meta_keys as $field) {
                     $meta[] = [
                         'key' => $field,
                         'value' => $query->query_vars['s'],
@@ -143,5 +181,24 @@ class AcfMetaSearch
         };
 
         add_filter('get_meta_sql', $func);
+    }
+
+    /**
+     * Recursively build a meta_key regular expression by appending parent
+     * field names to each field.
+     *
+     * @param array $field
+     *
+     * @return string
+     */
+    private function getMetaKeyRegex($field)
+    {
+        $parent = $field['parent'];
+
+        if (array_key_exists($parent, $this->fields) && $this->fields[$parent]['type'] == 'repeater') {
+            return $this->getMetaKeyRegex($this->fields[$parent]) . '_[0-9]+_' . $field['key'];
+        } else {
+            return $field['key'];
+        }
     }
 }
